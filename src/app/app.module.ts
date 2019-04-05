e6613b7d9b8021a4604941150e3ac207bb2aa5b1
pluto2001 <pitsamaplurao@gmail.com>
2019-04-05 14:56:01 +0700

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,35 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
+import { AngularFireModule } from "@angular/fire";
+import {
+  AngularFireDatabaseModule,
+  AngularFireDatabase
+} from "@angular/fire/database";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBW7f8-ZKYF70YR1t_3kLpabtUG7Rs6ojU",
+  authDomain: "ionic-5697d.firebaseapp.com",
+  databaseURL: "https://ionic-5697d.firebaseio.com",
+  projectId: "ionic-5697d",
+  storageBucket: "ionic-5697d.appspot.com",
+  messagingSenderId: "225306514297"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule
   ],
   providers: [
+    AngularFireDatabase,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AngularFireDatabase } from "@angular/fire/database";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
+  constructor(public fb: AngularFireDatabase) {}
+  ngOnInit() {
+    this.fb
+      .object("/DHT_NOW")
+      .valueChanges()
+      .subscribe((value: any) => {
+        console.log(value);
+      });
+  }
 }
